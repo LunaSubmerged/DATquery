c7ccac94eb4635eb0a0c58fb6e29031620aa0749
Chris Lewis <>
2024-06-13 14:34:55 +0200

    wip movepool work

---
+++ move_pool.py
@@ -0,0 +1,32 @@
+import requests
+import csv
+import inflection
+import discord
+
+from io import StringIO
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from database import Database
+
+
+data = requests.get("https://docs.google.com/spreadsheets/d/1XDqCQF4miFGGaY5tGTTAgTaZ7koKiqgPAB571fYbVt4/export?format=csv&gid=0")
+
+csv_file = StringIO(data.text)
+reader = csv.reader(csv_file)
+rows = list(reader)
+for row in rows:
+    row.pop(2)
+    row.pop(0)
+for count, row in enumerate(list(rows)):
+    print(count)
+    print(row[0])
+    if row[0] == "":
+        rows.pop(0)
+    else:
+        break
+
+keys = []
+for header in rows[0]:
+    keys.append(header)
+print(keys)
+rows.pop(0)
