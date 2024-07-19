b1927502b9c8fe02eba3e56cb8c3f3b475cdf7dc
HeliosAflame <alienswan02@gmail.com>
2024-07-19 13:34:56 +0200

    fix names and art for mega and primal

--- pokemon.py
+++ pokemon.py
@@ -35,11 +35,15 @@ class PokemonDatabase(Database):
         sanitized_row.pop("signature_moveor_moves")
         if sanitized_row["sprite_alias"] == "":
             sanitized_row["sprite_alias"] = name.lower().replace(" ", "")
+        else:
+            sanitized_row["sprite_alias"] = sanitized_row["sprite_alias"].lower()
 
         pokemon = Pokemon(**sanitized_row)
         if sanitized_row["id"] == "Mega":
+            pokemon.name = "Mega " + pokemon.name
             self.dictionary["mega_" + name.lower()] = pokemon
         elif sanitized_row["id"] == "Primal":
+            pokemon.name = "Primal " + pokemon.name
             self.dictionary["primal_" + name.lower()] = pokemon
         else:
             self.dictionary[name.lower()] = pokemon
@@ -58,14 +62,17 @@ class PokemonDatabase(Database):
           )
           embed.set_thumbnail(url = "https://play.pokemonshowdown.com/sprites/bw/" + pokemon.sprite_alias + ".png")
           embed.add_field(name="Abilities", value = pokemon.abilities)
-          embed.add_field(name="Hidden Ability", value = pokemon.hidden_ability)
+          if pokemon.hidden_ability != "":
+            embed.add_field(name="Hidden Ability", value = pokemon.hidden_ability)
           line1 = "HP: " + pokemon.hp
           line2 = "ATK: " + pokemon.atk + " | DEF: " + pokemon.defence + " | SpA: " + pokemon.sp_a + " | SpD: " + pokemon.sp_d
           line3 = "Speed: " + pokemon.spe
           line4 = "Size Class: " + pokemon.size
           line5 = "Weight Class: " + pokemon.weight
           embed.add_field(name="Stats", value = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5,  inline= False)
-          embed.add_field(name="Signature Move", value = pokemon.signature_move, inline=False)
-          embed.add_field(name="Traits", value = pokemon.traits)
+          if pokemon.signature_move != "":
+            embed.add_field(name="Signature Move", value = pokemon.signature_move, inline=False)
+          if pokemon.traits != "":
+            embed.add_field(name="Traits", value = pokemon.traits)
 
           return (embed)
