c4d765398f26d75a1b06057eeba4624e85205bdd
HeliosAflame <alienswan02@gmail.com>
2024-10-17 23:07:26 +0200

    add "-" to rolls command

--- pokemon_discord_bot.py
+++ pokemon_discord_bot.py
@@ -149,12 +149,16 @@ async def roll(ctx, arg="20d600"):
     modifier = 0
     if '+' in arg:
         arg, modifier = arg.split("+",1)
+        modifier = int(modifier)
+    elif '-' in arg:
+        arg, modifier = arg.split("-", 1)
+        modifier = -int(modifier)
     if index_of_d == 0:
-        await ctx.send(random.randint(1, int(arg[index_of_d + 1:]))+int(modifier))
+        await ctx.send(random.randint(1, int(arg[index_of_d + 1:]))+modifier)
     else:
         output = []
         for x in range(int(arg[:index_of_d])):
-            output.append(random.randint(1, int(arg[index_of_d + 1:]))+int(modifier))
+            output.append(random.randint(1, int(arg[index_of_d + 1:]))+modifier)
         str_output = str(output)
         str_output = str_output[1:-1]
         await ctx.send(str_output)
