3775136c9f2446335ef54d67a0f8ce8d6e2b7acf
HeliosAflame <alienswan02@gmail.com>
2025-01-22 20:58:29 +0100

    add duo's sprite dictionary

--- databases/pokemon_db.py
+++ databases/pokemon_db.py
@@ -41,7 +41,51 @@ class PokemonDatabase(Database):
         sanitized_row.pop("def")
         sanitized_row["signature_move"] = sanitized_row["signature_moveor_moves"]
         sanitized_row.pop("signature_moveor_moves")
-        if sanitized_row["showdown_alias"] == "":
+
+        aliases = {
+            'basculin-blue-striped': 'basculin-bluestriped',
+            'basculin-white-striped': 'basculin-whitestriped',
+            'charizard-mega-x': 'charizard-megax',
+            'charizard-mega-y': 'charizard-megay',
+            'chien-pao': 'chienpao',
+            'chi-yu': 'chiyu',
+            'darmanitan-galar-zen': 'darmanitan-galarzen',
+            "farfetch'd": 'farfetchd',
+            'gourgeist-small': 'gourgeist',
+            'gourgeist-average': 'gourgeist',
+            'gourgeist-large': 'gourgeist',
+            'gourgeist-super': 'gourgeist',
+            'greninja-bond': 'greninja',
+            'indeedee-m': 'indeedee',
+            'meowstic-m': 'meowstic',
+            'mewtwo-mega-x': 'mewtwo-megax',
+            'mewtwo-mega-y': 'mewtwo-megay',
+            'necrozma-dawn-wings': 'necrozma-dawnwings',
+            'necrozma-dusk-mane': 'necrozma-duskmane',
+            'oricorio-baile': 'oricorio',
+            "oricorio-pa'u": 'oricorio-pau',
+            'porygon-z': 'porygonz',
+            'pumpkaboo-small': 'pumpkaboo',
+            'pumpkaboo-average': 'pumpkaboo',
+            'pumpkaboo-large': 'pumpkaboo',
+            'pumpkaboo-super': 'pumpkaboo',
+            'rockruff-dusk': 'rockruff',
+            'rotom-spin': 'rotom-fan',
+            'rotom-freeze': 'rotom-frost',
+            'rotom-cut': 'rotom-mow',
+            "sirfetch'd": 'sirfetchd',
+            'tauros-paldea-combat': 'tauros-paldeacombat',
+            'tauros-paldea-blaze': 'tauros-paldeablaze',
+            'tauros-paldea-aqua': 'tauros-paldeaaqua',
+            'ting-lu': 'tinglu',
+            'toxtricity-low-key': 'toxtricity-lowkey',
+            'urshifu-rapid-strike': 'urshifu-rapidstrike',
+            'wo-chien': 'wochien',
+            'zygarde-10%': 'zygarde-10'
+        }
+        if name.lower() in aliases:
+            sanitized_row["showdown_alias"] = aliases[name.lower()]
+        elif sanitized_row["showdown_alias"] == "":
             sanitized_row["showdown_alias"] = name.lower().replace(" ", "")
         else:
             sanitized_row["showdown_alias"] = sanitized_row["showdown_alias"].lower()
